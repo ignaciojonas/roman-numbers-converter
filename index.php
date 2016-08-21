3dcca6d0bb237435f97da346f21c764ffdb37574
Ignacio Jonas <ignaciojonas@gmail.com>
2016-08-21 09:42:32 -0300

    Change include order

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-include 'lib/vendor/autoload.php';
 require_once __DIR__ .'/config/appConfig.php';
 require_once __DIR__ .'/controllers/RomanNumbersController.php';
+include 'lib/vendor/autoload.php';
+
 $controller = new RomanNumbersController();
 
 switch (isset($_GET[AppConfig::$ACTION]) ? $_GET[AppConfig::$ACTION] : null) {
