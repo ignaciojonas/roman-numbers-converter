57324b716b435ccc081cad80516081f33c0ce2e1
Ignacio Jonas <ignaciojonas@gmail.com>
2016-08-21 10:09:47 -0300

    Add Profile

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
-// include 'lib/vendor/autoload.php';
-// require_once '/config/appConfig.php';
-// require_once '/controllers/RomanNumbersController.php';
-//
-//
-// $controller = new RomanNumbersController();
-//
-// switch (isset($_GET[AppConfig::$ACTION]) ? $_GET[AppConfig::$ACTION] : null) {
-//   case AppConfig::$ACTION_CONVERT:
-//       $controller->convertNumber();
-//       break;
-//   default:
-//       $controller->showPage();
-//       break;
-// }
-phpinfo();
+include 'lib/vendor/autoload.php';
+include 'config/appConfig.php';
+include 'controllers/RomanNumbersController.php';
+$controller = new RomanNumbersController();
+
+switch (isset($_GET[AppConfig::$ACTION]) ? $_GET[AppConfig::$ACTION] : null) {
+  case AppConfig::$ACTION_CONVERT:
+      $controller->convertNumber();
+      break;
+  default:
+      $controller->showPage();
+      break;
+}
+
 ?>
