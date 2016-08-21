2fc9ca4992cb53245033ce38d2198ae971fb94c2
Ignacio Jonas <ignaciojonas@gmail.com>
2016-08-21 09:23:56 -0300

    Update relative paths

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include 'lib/vendor/autoload.php';
-include 'config/appConfig.php';
-include 'controllers/RomanNumbersController.php';
+require_once __DIR__ .'/config/appConfig.php';
+require_once __DIR__ .'/controllers/RomanNumbersController.php';
 $controller = new RomanNumbersController();
 
 switch (isset($_GET[AppConfig::$ACTION]) ? $_GET[AppConfig::$ACTION] : null) {
